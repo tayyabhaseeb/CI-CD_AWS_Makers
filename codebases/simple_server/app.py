433a6a3c203af13b74dd367c8d438d323c6cc307
Tayyab Haseeb <tayyabhaseeb5@gmail.com>
2025-11-16 23:46:39 +0000

    latest

--- codebases/simple_server/app.py
+++ codebases/simple_server/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def get_home():
-    return "Hello, world!"
+    return "Hello from Humza"
 
 if __name__ == '__main__':
     app.run(
@@ -13,3 +13,5 @@ if __name__ == '__main__':
       port=5001,
       host="0.0.0.0" # Listen for connections _to_ any server
     )
+
+
